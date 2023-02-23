2366aa311fa7d408960644b3788d1ff9e4f25e49
virat1816 <116003414+virat1816@users.noreply.github.com>
2023-02-23 20:44:51 +0530

    Add files via upload

---
+++ check_n.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+main(){
+	
+	int a,b;
+	
+	printf("Enter value of A :");
+	scanf("%d",&a);
+	
+	printf("Enter value of B :");
+	scanf("%d",&b);
+	
+	if(a<b){
+		printf("A value is less Then B");
+	} else{
+		printf("A value is greater Then B");
+	}
+}
---
+++ natural_no.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+ main(){
+ 	
+    int num;
+    
+    printf("Enter Number: ");
+    scanf("%d",&num);
+ 
+    if(num == 0)
+    	 printf("This Number positive nor negative");
+	else if(num < 0)
+		printf("Negative");
+	else
+		printf("Positive");
+	
+}
+        
+       
+    
+        
+    
+     
+    
+
